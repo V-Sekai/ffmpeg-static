fa9115610434aa4b6b501f6565ecf29d4ff3598a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-09-21 09:15:06 -0700

    Comment out av1.

--- build.sh
+++ build.sh
@@ -242,14 +242,17 @@ download \
   "689dc495b22c5f08246c00dab35f1dc7" \
   "https://github.com/xiph/vorbis/archive/"
 
-rm -rf FFmpeg
-git clone https://github.com/FFmpeg/FFmpeg FFmpeg
-
 download \
-  "v0.8.5.tar.gz" \
-  "SVT-AV1-0.8.5.tar.gz" \
-  "bd5d4a9257565d451415d4fda3e5e3c7" \
-  "https://github.com/AOMediaCodec/SVT-AV1/archive"
+  "n4.3.1.tar.gz" \
+  "ffmpeg-4.3.1.tar.gz" \
+  "426ca412ca61634a248c787e29507206" \
+  "https://github.com/FFmpeg/FFmpeg/archive"
+
+# download \
+#   "v0.8.5.tar.gz" \
+#   "SVT-AV1-0.8.5.tar.gz" \
+#   "bd5d4a9257565d451415d4fda3e5e3c7" \
+#   "https://github.com/AOMediaCodec/SVT-AV1/archive"
 
 [ $download_only -eq 1 ] && exit 0
 
@@ -307,13 +310,13 @@ cd $BUILD_DIR/vorbis*
 make -j $jval
 make install
 
-echo "*** Building SVT-AV1 ***"
-cd $BUILD_DIR/SVT-AV1*
-mkdir -p build
-cd build
-[ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$TARGET_DIR -DBUILD_SHARED_LIBS= .. 
-cmake --build . --target install
+# echo "*** Building SVT-AV1 ***"
+# cd $BUILD_DIR/SVT-AV1*
+# mkdir -p build
+# cd build
+# [ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
+# cmake -DCMAKE_INSTALL_PREFIX:PATH=$TARGET_DIR -DBUILD_SHARED_LIBS= .. 
+# cmake --build . --target install
 
 cd $BUILD_DIR/SVT-AV1*
 ./Build/linux/build.sh --prefix $TARGET_DIR --static -s Windows release
@@ -344,7 +347,7 @@ PKG_CONFIG_PATH="$TARGET_DIR/lib/pkgconfig" ./configure \
   --enable-decoder=libopus --enable-decoder=opus \
   --enable-decoder=vp9 \
   --enable-decoder=vp8 \
-  --enable-libsvtav1 \
+  # --enable-libsvtav1 \
   --enable-decoder=libvorbis --enable-decoder=vorbis \
   --enable-parser=vp9 --enable-parser=opus \
   --enable-parser=vorbis \
