65a66b5a5ceb0ca0e5e7f55c3724a9f438008551
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-09-20 20:57:16 -0700

    Restore old vorbis config.

--- build.sh
+++ build.sh
@@ -298,7 +298,7 @@ echo "*** Building libvorbis ***"
 cd $BUILD_DIR/vorbis*
 [ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
 ./autogen.sh
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared --with-pic $cc_flags \
+./configure --prefix=$TARGET_DIR --disable-shared --with-pic $cc_flags \
   --disable-oggtest --disable-examples --disable-docs $cc_dep_lib_extra
 make -j $jval
 make install
